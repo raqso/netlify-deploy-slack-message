87dc3c816e809671b68a7d50d77d7d968cd4c9d5
raqso <raqsasg@gmail.com>
2020-09-05 19:22:10 +0200

    Add test route

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { slackHook } from './slackHook';
 const app = express()
 const PORT : string|number = process.env.PORT || 5000;
 
+app.use("/test", (req, res) => res.send("It's working"));
 app.use("*", slackHook);
 
 app.listen(PORT,() => console.log(`hosting @${PORT}`));
