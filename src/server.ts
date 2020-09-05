5ce218167596de730c604baa5194fc9ec8e55db7
raqso <raqsasg@gmail.com>
2020-09-05 19:47:51 +0200

    Parse JSON from requests

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import { slackHook } from './slackHook';
 const app = express()
 const PORT : string|number = process.env.PORT || 5000;
 
+app.use(express.json());
+
 app.use(`/${process.env.URL_TOKEN}`, slackHook);
 
 app.listen(PORT,() => console.log(`hosting @${PORT}`));
